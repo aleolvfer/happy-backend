cefd58f757ce8372d5945ff8441e8808b9bdf786
aleolvfer <aleolvfer@hotmail.com>
2020-10-20 01:40:38 -0300

    Criando orfanato (sem imagem)

--- src/database/migrations/1603067946424-create_orphanages.ts
+++ src/database/migrations/1603067946424-create_orphanages.ts
@@ -39,6 +39,10 @@ export class createOrphanages1603067946424 implements MigrationInterface {
           type: 'text'
         },
         {
+          name: 'opening_hours',
+          type: 'varchar'
+        },
+        {
           name: 'open_on_weekends',
           type: 'boolnean',
           default: false
